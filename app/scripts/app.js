b6356085d0c9af8454f95b82b3cfc46641f5cf24
Martin Flodin <mflodin@gmail.com>
2014-03-25 00:53:51 +0100

    Connected to Soundcloud

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,9 @@
 'use strict';
 define([
-        'Graph',
-        'StringInstrument',
-        'Waveform'
+        'http://connect.soundcloud.com/sdk.js'
     ],
     function(
-        Graph,
-        StringInstrument,
-        Waveform
+
     ) {
 
         window.requestAnimFrame = (function() {
@@ -25,26 +21,56 @@ define([
 
         window.AudioContext = window.AudioContext || window.webkitAudioContext;
         var audioContext = new window.AudioContext();
-        var SCALING_FACTOR = 12;
+        var scalingFactor = 12;
 
         var analyser = audioContext.createAnalyser();
         analyser.fftSize = 64;
         var frequencyData = new Uint8Array(analyser.frequencyBinCount);
         var mainCanvas = document.getElementById('mainCanvas');
         var mainContext = mainCanvas.getContext('2d');
-        var spectrumCanvas = document.getElementById('spectrumCanvas');
+        var spectrumCanvas = document.createElement('canvas');
         var spectrumContext = spectrumCanvas.getContext('2d');
-        var lightCanvas = document.getElementById('lightCanvas');
+        var lightCanvas = document.createElement('canvas');
         var lightContext = lightCanvas.getContext('2d');
         var spectrumImageData = spectrumContext.getImageData(0, 0, spectrumCanvas.width, spectrumCanvas.height);
         var lightImageData = lightContext.getImageData(0, 0, lightCanvas.width, lightCanvas.height);
+        var lineWidth = 10;
 
 
         function connectAudio() {
             var audio = document.getElementById('audio');
+            var attributionLink = document.getElementById('attribution');
+            var client_id = "d6fde5b15e7dd0e9006ce8d25efb5bec";
+
+            SC.initialize({
+              client_id: client_id
+            });
+
+
+            // SC.get("/tracks/141233519", {}, function(sound) { // testtrack - more power from start
+            SC.get("/tracks/17611741", {}, function(sound) { // darkside
+                audio.src = sound.stream_url + '?client_id=' + client_id;
+                createAttribution(sound);
+            });
+
             var source = audioContext.createMediaElementSource(audio); // creates a sound source
             source.connect(analyser);
             analyser.connect(audioContext.destination); // connect the source to the audioContext's destination (the speakers)
+        }
+
+        function createAttribution(sound) {
+            var container = document.getElementById('soundAttribution');
+            var userLink = document.createElement('a');
+            var by = document.createElement('span');
+            var soundLink = document.createElement('a');
+            userLink.innerHTML = sound.user.username;
+            userLink.href = sound.user.permalink_url;
+            by.innerHTML = ' by ';
+            soundLink.innerHTML = sound.title;
+            soundLink.href = sound.permalink_url;
+            container.appendChild(soundLink);
+            container.appendChild(by);
+            container.appendChild(userLink);
         }
 
         function renderFrame() {
@@ -60,6 +86,8 @@ define([
             spectrumCanvas.height = lightCanvas.height = mainCanvas.height / 2;
             spectrumCanvas.width = window.innerWidth / 2;
             lightCanvas.width = window.innerWidth / 2;
+            lineWidth = Math.max(Math.min(window.innerWidth/100, 10), 2); // scale between 2px and 10px depending on screen size;
+            scalingFactor = 100/lineWidth;
             // drawLight();
             console.log(mainCanvas.width, mainCanvas.height,
                 spectrumCanvas.width, spectrumCanvas.height,
@@ -86,16 +114,24 @@ define([
             mainContext.lineTo(w2 - tw2, th + p);
             mainContext.closePath();
 
+
+
             var opacity = spectrumData.totalMagnitude / spectrumData.maxMagnitude;
             mainContext.strokeStyle = 'rgba(255,255,255,'+ opacity + ')';
             // mainContext.strokeStyle = 'rgba(255,255,255,1)';
-            mainContext.lineWidth = 10;
+            mainContext.lineWidth = lineWidth;
             mainContext.shadowColor = '#fff';
             mainContext.shadowBlur = 40;
             mainContext.shadowOffsetX = 0;
             mainContext.shadowOffsetY = 0;
             //for (var i = spectrumData.totalMagnitude / 100; i > 0; i--) {
                 mainContext.stroke();
+
+
+            mainContext.moveTo(w2, p);
+            mainContext.lineTo(w2 + tw2, th + p);
+            mainContext.lineTo(w2 - tw2, th + p);
+            mainContext.closePath();
             //};
             mainContext.restore();
         }
@@ -105,14 +141,13 @@ define([
             if(!spectrumData) {
                 return;
             }
-            var lineHeight = 10;
             var opacity = spectrumData.totalMagnitude / spectrumData.maxMagnitude;
             lightContext.fillStyle = 'rgba(255,255,255,'+ opacity + ')';
             lightContext.shadowColor = '#fff';
             lightContext.shadowBlur = 40;
             lightContext.shadowOffsetX = 0;
             lightContext.shadowOffsetY = 0;
-            lightContext.fillRect(0, (lightCanvas.height - lineHeight) / 2, lightCanvas.width,  lineHeight);
+            lightContext.fillRect(0, (lightCanvas.height - lineWidth) / 2, lightCanvas.width,  lineWidth);
         }
 
         var maxMagnitude = 255;
@@ -140,7 +175,7 @@ define([
                 totalMagnitude += frequencyData[i];
             }
 
-            totalMagnitude /= SCALING_FACTOR;
+            totalMagnitude /= scalingFactor;
             maxMagnitude = Math.max(maxMagnitude, totalMagnitude);
 
             return {
@@ -172,7 +207,7 @@ define([
 
             for (var i = 0; i < spectrumData.bins.length; i++) {
                 // multiply spectrum by a zoom value
-                var magnitude = spectrumData.bins[i] / SCALING_FACTOR;
+                var magnitude = spectrumData.bins[i] / scalingFactor;
                 // Draw rectangle bars for each frequency bin
                 spectrumContext.fillStyle = spectrumData.colors[Math.floor(i)];
                 spectrumContext.shadowColor = spectrumContext.fillStyle;
@@ -220,7 +255,7 @@ define([
             mainContext.drawImage(lightCanvas, -lightCanvas.width,0);
             mainContext.restore();
 
-            mainContext.translate(0,10);
+            mainContext.translate(0,lineWidth);
             drawTriangle(spectrumData);
             mainContext.restore();
         }
