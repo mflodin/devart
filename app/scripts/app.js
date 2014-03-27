a5a8d5bf50b96548599b53eaf61f81d16f338be0
Martin Flodin <mflodin@gmail.com>
2014-03-27 03:16:45 +0100

    Added inner light triangle

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,6 +25,7 @@ define([
 
         var analyser = audioContext.createAnalyser();
         analyser.fftSize = 64;
+        // analyser.smoothingTimeConstant = 0.95;
         var frequencyData = new Uint8Array(analyser.frequencyBinCount);
         var mainCanvas = document.getElementById('mainCanvas');
         var mainContext = mainCanvas.getContext('2d');
@@ -35,6 +36,9 @@ define([
         var spectrumImageData = spectrumContext.getImageData(0, 0, spectrumCanvas.width, spectrumCanvas.height);
         var lightImageData = lightContext.getImageData(0, 0, lightCanvas.width, lightCanvas.height);
         var lineWidth = 10;
+        var lightPosition = 0.45;
+        var mainTriangle;
+        var innerTriangleGradient;
 
 
         function connectAudio() {
@@ -87,70 +91,101 @@ define([
             spectrumCanvas.width = window.innerWidth / 2;
             lightCanvas.width = window.innerWidth / 2;
             lineWidth = Math.max(Math.min(window.innerWidth/100, 10), 2); // scale between 2px and 10px depending on screen size;
-            scalingFactor = 100/lineWidth;
-            // drawLight();
-            console.log(mainCanvas.width, mainCanvas.height,
-                spectrumCanvas.width, spectrumCanvas.height,
-                lightCanvas.width, lightCanvas.height);
+            mainTriangle = getMainTriangle({x: mainCanvas.width * 0.5, y: 0}, spectrumCanvas.height);
+
+
         };
         onResize();
         window.onresize = onResize;
+        
+        function drawInnerTriangle(mainTriangle, spectrumData) {
 
-        function drawTriangle (spectrumData) {
-            var h = mainCanvas.height;
-            var w = mainCanvas.width;
-            var w2 = w/2;
-            // var tw = spectrumCanvas.height;
-            var th = spectrumCanvas.height;
-            var tw = th / (Math.sqrt(3)/2);
-            var tw2 = tw/2;
-            var p = 0;
-            // var th = Math.sqrt(3)/2 * tw;
+            var innerTriangle = getInnerTriangle(mainTriangle, spectrumData.intensity);
 
             mainContext.save();
-            mainContext.beginPath();
-            mainContext.moveTo(w2, p);
-            mainContext.lineTo(w2 + tw2, th + p);
-            mainContext.lineTo(w2 - tw2, th + p);
-            mainContext.closePath();
-
-
-
-            var opacity = spectrumData.totalMagnitude / spectrumData.maxMagnitude;
-            mainContext.strokeStyle = 'rgba(255,255,255,'+ opacity + ')';
-            // mainContext.strokeStyle = 'rgba(255,255,255,1)';
-            mainContext.lineWidth = lineWidth;
+            // mainContext.globalCompositeOperation = 'copy';
+            var midPointFarSide = getPointOnLine(innerTriangle.c, innerTriangle.b, 0.25);
+            // mainContext.strokeStyle = 'rgba(255,255,255,'+ spectrumData.intensity + ')';
+            innerTriangleGradient = mainContext.createLinearGradient(innerTriangle.a.x, innerTriangle.a.y, midPointFarSide.x, midPointFarSide.y);
+            // innerTriangleGradient = mainContext.createLinearGradient(innerTriangle.a.x, innerTriangle.a.y, (innerTriangle.b.x - innerTriangle.c.x) * 0.5 + innerTriangle.a.x,  innerTriangle.a.y);
+            innerTriangleGradient.addColorStop(0, 'rgba(255,255,255,' + spectrumData.intensity + ')');
+            innerTriangleGradient.addColorStop(0.5 + spectrumData.intensity * 0.5, 'rgba(255,255,255,0)');
+            mainContext.fillStyle = innerTriangleGradient;//'rgba(255,255,255,1)';
             mainContext.shadowColor = '#fff';
             mainContext.shadowBlur = 40;
             mainContext.shadowOffsetX = 0;
             mainContext.shadowOffsetY = 0;
-            //for (var i = spectrumData.totalMagnitude / 100; i > 0; i--) {
-                mainContext.stroke();
-
-
-            mainContext.moveTo(w2, p);
-            mainContext.lineTo(w2 + tw2, th + p);
-            mainContext.lineTo(w2 - tw2, th + p);
+            mainContext.beginPath();
+            mainContext.moveTo(innerTriangle.a.x, innerTriangle.a.y);
+            mainContext.lineTo(innerTriangle.b.x, innerTriangle.b.y);
+            mainContext.lineTo(innerTriangle.c.x, innerTriangle.c.y);
             mainContext.closePath();
-            //};
+            mainContext.fill();
+
+            mainContext.restore();
+        }
+
+        function getPointOnLine(startPoint, endPoint, fraction) {
+            return {
+                x: (endPoint.x - startPoint.x) * fraction + startPoint.x,
+                y: (endPoint.y - startPoint.y) * fraction + startPoint.y
+            }
+        }
+
+        function getInnerTriangle(outerTriangle, spread) {
+            var a = getPointOnLine(outerTriangle.a, outerTriangle.c, lightPosition);
+            a.x -= lineWidth * 0.5;
+            var b = getPointOnLine(outerTriangle.a, outerTriangle.b, 0.5 + spread * 0.5);
+            var c = getPointOnLine(outerTriangle.b, outerTriangle.a, 0.5 + spread * 0.5);
+
+            return {a: a, b: b, c: c};
+        }
+
+        function getMainTriangle(startPoint, height) {
+            var halfWidth = height / (Math.sqrt(3)); // the height / side relation of a unilateral triangle
+            var a = {x: startPoint.x, y: startPoint.y};
+            var b = {x: startPoint.x + halfWidth, y: height + startPoint.y};
+            var c = {x: startPoint.x - halfWidth, y: height + startPoint.y};
+
+            return {a: a, b: b, c: c};
+        }
+        
+        function drawTriangle (spectrumData) {
+            var opacity = spectrumData.intensity;
+
+            mainContext.save();
+            mainContext.globalCompositeOperation = 'copy';
+            mainContext.strokeStyle = 'rgba(255,255,255,'+ opacity + ')';
+            mainContext.shadowColor = '#fff';
+            mainContext.shadowBlur = 40;
+            mainContext.shadowOffsetX = 0;
+            mainContext.shadowOffsetY = 0;
+            mainContext.lineWidth = lineWidth;
+            mainContext.beginPath();
+            mainContext.moveTo(mainTriangle.a.x, mainTriangle.a.y);
+            mainContext.lineTo(mainTriangle.b.x, mainTriangle.b.y);
+            mainContext.lineTo(mainTriangle.c.x, mainTriangle.c.y);
+            // mainContext.lineJoin = 'round';
+            mainContext.closePath();
+            mainContext.stroke();
             mainContext.restore();
         }
         
         function drawLight(spectrumData) {
-            lightContext.clearRect(0,0,lightCanvas.width, lightCanvas.height);
+            lightContext.clearRect(0,lightCanvas.height * 0.25,lightCanvas.width, lightCanvas.height * 0.75);
             if(!spectrumData) {
                 return;
             }
-            var opacity = spectrumData.totalMagnitude / spectrumData.maxMagnitude;
+            var opacity = spectrumData.intensity;
             lightContext.fillStyle = 'rgba(255,255,255,'+ opacity + ')';
             lightContext.shadowColor = '#fff';
             lightContext.shadowBlur = 40;
             lightContext.shadowOffsetX = 0;
             lightContext.shadowOffsetY = 0;
-            lightContext.fillRect(0, (lightCanvas.height - lineWidth) / 2, lightCanvas.width,  lineWidth);
+            lightContext.fillRect(0, (lightCanvas.height - lineWidth * 0.5) * lightPosition, lightCanvas.width,  lineWidth);
         }
 
-        var maxMagnitude = 255;
+        var maxMagnitude  = spectrumCanvas.height * scalingFactor;
         function getSpectrumData(frequencyData) {
             // body...
             var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo'];
@@ -175,31 +210,34 @@ define([
                 totalMagnitude += frequencyData[i];
             }
 
-            totalMagnitude /= scalingFactor;
             maxMagnitude = Math.max(maxMagnitude, totalMagnitude);
+            scalingFactor = maxMagnitude / spectrumCanvas.height;
 
             return {
                 maxMagnitude: maxMagnitude,
                 totalMagnitude: totalMagnitude,
+                intensity : totalMagnitude / maxMagnitude,
                 bins : spectrumData,
                 colors : colors
             };
         }
 
+
+
+
+
         var xStepWidth = 2;
-
-
         function drawSpectrum(spectrumData) {
             if(!spectrumData) {
                 return;
             }
             var y = 0;
-            spectrumContext.clearRect(0, 0, spectrumCanvas.width,  spectrumCanvas.height);
+            spectrumContext.clearRect(0, 0, xStepWidth,  spectrumCanvas.height);
 
             spectrumContext.putImageData(spectrumImageData, xStepWidth,  0); // draw last frame's data displaced by xStepWidth
             spectrumContext.save();
             spectrumContext.translate(xStepWidth,  spectrumCanvas.height / 2);
-            spectrumContext.globalAlpha = spectrumData.totalMagnitude / spectrumData.maxMagnitude;
+            spectrumContext.globalAlpha = spectrumData.intensity;
             spectrumContext.shadowBlur = 40;
             spectrumContext.shadowOffsetX = 0;
             spectrumContext.shadowOffsetY = 0;
@@ -207,21 +245,16 @@ define([
 
             for (var i = 0; i < spectrumData.bins.length; i++) {
                 // multiply spectrum by a zoom value
-                var magnitude = spectrumData.bins[i] / scalingFactor;
+                var scaledMagnitude = spectrumData.bins[i] / scalingFactor;
                 // Draw rectangle bars for each frequency bin
                 spectrumContext.fillStyle = spectrumData.colors[Math.floor(i)];
                 spectrumContext.shadowColor = spectrumContext.fillStyle;
-                spectrumContext.fillRect(0, 0 - spectrumData.totalMagnitude / 2 + y, xStepWidth,  magnitude);
-                y += magnitude;
+                spectrumContext.fillRect(0, 0 - spectrumData.totalMagnitude / (2 * scalingFactor) + y, xStepWidth,  scaledMagnitude);
+                y += scaledMagnitude;
             };
 
 
             spectrumContext.restore();
-            // spectrumContext.clearRect(0, 0, xStepWidth,  spectrumCanvas.height);
-            // spectrumContext.shadowBlur = null;
-            // spectrumContext.shadowColor = null;
-
-            
             spectrumImageData = spectrumContext.getImageData(0, 0, spectrumCanvas.width - xStepWidth,  spectrumCanvas.height);
             
 
@@ -238,12 +271,10 @@ define([
             // debugDraw();
             drawSpectrum(spectrumData);
 
-
-
             mainContext.save();
             mainContext.translate(mainCanvas.width/2, 0);
             mainContext.transform(1, verticalShear, horizontalShear, 1, 0, 0);
-            // mainContext.translate(-10 * horizontalShear, 10);
+            mainContext.translate(-lineWidth * horizontalShear - 2, lineWidth); // ok to be higher
 
             mainContext.drawImage(spectrumCanvas, -2,0);
             mainContext.restore();
@@ -251,31 +282,36 @@ define([
             mainContext.save();
             mainContext.translate(mainCanvas.width/2, 0);
             mainContext.transform(1, -verticalShear, -horizontalShear, 1, 0, 0);
-            // mainContext.translate(10 * horizontalShear, 10);
+            mainContext.translate(lineWidth * horizontalShear + 2, lineWidth * 2);
             mainContext.drawImage(lightCanvas, -lightCanvas.width,0);
             mainContext.restore();
 
-            mainContext.translate(0,lineWidth);
+            mainContext.translate(0,lineWidth * 2);
+            drawInnerTriangle(mainTriangle, spectrumData);
             drawTriangle(spectrumData);
             mainContext.restore();
         }
 
         function debugDraw () {
+            mainContext.save();
+            mainContext.translate(0,lineWidth * 2);
+            drawLight({intensity: 0.5});
+            drawTriangle({intensity: 0.5});
+            drawInnerTriangle(mainTriangle, {intensity: 0.5});
+            mainContext.restore();
             spectrumContext.clearRect(0,0,spectrumCanvas.width, spectrumCanvas.height);
             spectrumContext.fillStyle ='rgba(255,0,0,0.5)';
             spectrumContext.fillRect(0,0,spectrumCanvas.width,spectrumCanvas.height);
             lightContext.fillStyle ='rgba(0,255,0,0.5)';
 
             lightContext.fillRect(0,0,lightCanvas.width,lightCanvas.height);
-            mainContext.fillStyle ='rgba(0,0,255,0.5)';
+            mainContext.fillStyle = 'rgba(0,0,255,0.5)';
+            // mainContext.fillStyle = innerTriangleGradient;
             mainContext.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
-            mainContext.save();
-            mainContext.translate(0,10);
-            drawTriangle({totalMagnitude: 1, maxMagnitude : 1});
-            mainContext.restore();
         }
 
         connectAudio();
-        drawLight();
+        drawComposite({intensity: 0.25, bins: []});
         renderFrame();
+        // debugDraw();
     });
